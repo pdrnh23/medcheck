ad106b6c797d8a0a6a44e6f2485d8f6ff62d3eda
Pedro Perrota <pepemonster2003@hotmail.com>
2024-11-13 17:28:29 -0300

    página inicial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+<header>
+  <div class="bg-teal-900 w-full h-24 flex items-center justify-center">
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+      <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Med</span>Check
+    </h1>
+  </div>
+  <div class="bg-teal-800	w-full h-48 flex items-center justify-center">
+    <img class="size-28" src="./assets/img/relogio.png">
+  </div>
+</header>
+</template>
+
+<style scoped>
+</style>
+
